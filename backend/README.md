90976d8179c82ddc0139b0589792f03dac25431d
Vincent <lywc420@gmail.com>
2026-05-20 11:56:40 -0700

    adds embedding script

--- backend/README.md
+++ backend/README.md
@@ -1,1 +1,16 @@
+# Backend
 
+Handles chunking, embeddings, and response generation.
+Client side handles retrieval
+
+## Requirements
+
+- Textsplitter
+- Setence transformer
+
+## Steps
+
+1. Setup a markdown file
+2. Chunk the markdown file using the text splitter library from langchain
+3. Create embeddings using sentence transformer
+4. Writes embeddings to embeddings.json
---
+++ backend/scripts/build_embeddings.py
@@ -0,0 +1,59 @@
+"""
+Script to chunk markdown files and convert to static embeddings
+"""
+
+from langchain_text_splitters import (
+    MarkdownHeaderTextSplitter,
+    RecursiveCharacterTextSplitter,
+)
+from langchain_community.document_loaders import TextLoader
+from sentence_transformers import SentenceTransformer
+import orjson
+
+# Load CV
+loader = TextLoader("cv.md", encoding="utf-8")
+docs = loader.load()
+
+
+# Extract raw markdown text
+markdown_text = docs[0].page_content
+
+
+# Split markdown by headers
+headers_to_split_on = [
+    ("#", "Header 1"),
+    ("##", "Header 2"),
+    ("###", "Header 3"),
+]
+
+markdown_splitter = MarkdownHeaderTextSplitter(
+    headers_to_split_on=headers_to_split_on
+)
+
+header_splits = markdown_splitter.split_text(markdown_text)
+
+
+# Further split into smaller chunks
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=300,
+    chunk_overlap=50
+)
+
+chunks = text_splitter.split_documents(header_splits)
+
+
+# Extract chunk text
+chunk_texts = [chunk.page_content for chunk in chunks]
+
+
+# Load embedding model
+model = SentenceTransformer(
+    "sentence-transformers/all-MiniLM-L6-v2"
+)
+
+# Generate embeddings
+embeddings = model.encode(chunk_texts, convert_to_numpy=True).tolist()
+print(f"Number of chunks: {len(chunk_texts)}")
+
+with open("embeddings.json", "wb") as f:
+    f.write(orjson.dumps(embeddings))
